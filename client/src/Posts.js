dde652bd91697362bf41abc3e15c6c7a6bfdbf4d
Ben Koehler <bkoehler85282@gmail.com>
2020-03-11 11:39:23 -0700

    completed mvp with stretch and styling

---
+++ client/src/Posts.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+class Posts extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:9000/api/posts")
+      .then(res => res.json())
+      .then(posts =>
+        this.setState({ posts }, () => console.log("Posts fetched...", posts))
+      );
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          {this.state.posts.map(post => (
+            <Col xs="6" key={post.id}>
+              <Row>{post.title}</Row>
+              <Row>{post.contents}</Row>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Posts;
