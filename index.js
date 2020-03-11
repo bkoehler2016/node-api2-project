703b176d4a7d5f31af19573bf25d2fb6a9cc0439
Ben Koehler <bkoehler85282@gmail.com>
2020-03-11 11:20:39 -0700

    setup client and installed cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const postsRouter = require("./posts/posts-router.js");
 const server = express();
+const cors = require("cors");
 
 server.use(express.json());
+server.use(cors());
 server.use("/api/posts", postsRouter);
 
 const port = 9000;
