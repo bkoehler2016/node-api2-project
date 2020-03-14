8f4a81a01f587d674484714a0a19396ea9c78f21
Ben Koehler <bkoehler85282@gmail.com>
2020-03-13 23:42:59 -0700

    added a method to delete post by id

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -16,18 +16,44 @@ class Posts extends Component {
     });
   }
 
+  handleChange = event => {
+    this.setState({ id: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    axios
+      .delete(`http://localhost:9000/api/posts/${this.state.id}`)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+  };
+
   render() {
     return (
       <Container>
         <Row>
           {this.state.posts.map(post => (
             <Col xs="6" key={post.id}>
+              <Row>ID: {post.id}</Row>
               <Row>Title: {post.title}</Row>
               <Row>Comment: {post.contents}</Row>
-              <Button color="danger">Delete</Button>
             </Col>
           ))}
         </Row>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Post ID:
+            <input type="text" name="id" onChange={this.handleChange} />
+          </label>
+
+          <Button color="danger" type="submit">
+            {" "}
+            Delete
+          </Button>
+        </form>
       </Container>
     );
   }
